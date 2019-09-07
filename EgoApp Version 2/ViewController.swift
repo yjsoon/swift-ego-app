f0136ab1b05ee78e87e1a40df193532f00251cda
YJ Soon <yjsoon@tinkertanker.com>
2019-09-07 09:16:37 +0800

    Initial Commit

---
+++ EgoApp Version 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EgoApp Version 2
+//
+//  Created by Soon Yin Jie on 7/9/19.
+//  Copyright © 2019 Tinkercademy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
