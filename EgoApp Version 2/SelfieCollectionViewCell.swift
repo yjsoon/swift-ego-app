a1281ca60ea5807170cd8c92b0768e9e69bab360
YJ Soon <yjsoon@tinkertanker.com>
2019-09-07 09:42:48 +0800

    Set up collection view controller with demo data

---
+++ EgoApp Version 2/SelfieCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  SelfieCollectionViewCell.swift
+//  EgoApp Version 2
+//
+//  Created by Soon Yin Jie on 7/9/19.
+//  Copyright © 2019 Tinkercademy. All rights reserved.
+//
+
+import UIKit
+
+class SelfieCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoLabel: UILabel! 
+    
+}
